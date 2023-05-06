b04beec29f6aa789e1e17a70cbf61363d3dc59ab
Tharun <kumartharun435@gmail.com>
2023-05-06 18:14:23 +0530

    Added Variables

---
+++ Basics/Variables/Variables.java
@@ -0,0 +1,8 @@
+class Variables {
+    public static void main(String args[]) {
+        System.out.println("Exploring Variables");
+        int n = -1;
+        int p = 3;
+        System.out.println(n + p);
+    }
+}
