97978b0be6af774f1a61004c78498aba350ed01c
Tharun <kumartharun435@gmail.com>
2023-05-07 20:15:25 +0530

    Added Loops

---
+++ Basics/Loops/Loops.java
@@ -0,0 +1,18 @@
+class Loops {
+    public static void main(String args[]) {
+        int i = 1;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+        // If the requirement is executing atleast once even if the condition fails
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 10);
+        for(int j = 0; j <= 10; j++)
+        {
+            System.out.println(j);
+        }
+    }
+}
